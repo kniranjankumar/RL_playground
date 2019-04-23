b9655f9a9bbf56f92cbad6b92675cf5ef9caba89
Niranjan <kniranjankumar.eee@gmail.com>
2019-04-22 23:35:55 -0400

    "reward_scaling added on env side"

--- stable_baselines/check_arm.py
+++ stable_baselines/check_arm.py
@@ -83,8 +83,8 @@ class NetworkVecEnv(SubprocVecEnv):
                         rnn_input1 = [tf.concat([obs[i+1], act[i]],axis=1) for i in range(len(act))]
                         rnn_input1 = tf.concat(rnn_input1, axis=0)
                         rnn_input1 = tf.reshape(rnn_input1,shape=[-1,self.act_dim+self.obs_dim])
-                        rnn_input = slim.fully_connected(rnn_input1, 128, scope='in1')
-                        rnn_input = tf.reshape(rnn_input,shape=[self.num_steps,-1 ,128])
+                        # rnn_input = slim.fully_connected(rnn_input1, 128, scope='in1')
+                        rnn_input = tf.reshape(rnn_input1,shape=[self.num_steps,-1 ,rnn_input1.get_shape().as_list()[-1]])
                         rnn_input_ = tf.unstack(rnn_input, axis=0)
                         c0 = slim.fully_connected(obs[0], 64, scope='c0')
                         m0 = slim.fully_connected(obs[0], 64, scope='m0')
