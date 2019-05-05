6d23fb53627feb4eb00f888bfdc9f1a38ac9da78
Niranjan <kniranjankumar.eee@gmail.com>
2019-05-05 19:04:54 -0400

    "simulation failure management"

--- DartEnv2/gym/envs/dart/KR5_arm_acceleration.py
+++ DartEnv2/gym/envs/dart/KR5_arm_acceleration.py
@@ -626,23 +626,21 @@ class ControllerOCPose:
             # self.target_x = self.end_effector.to_world(self.end_effector_offset)
             self.skel.set_velocities(0*self.skel.dq)
             # self.skel.set_accelerations(0*self.skel.dq)
-            try:
-                # if np.any(np.isnan(self.box.dq)):
-                #     self.skel.set_positions(self.start)
-                #     self.skel.world.complete = True
-                #     self.flipped = False
-                #     self.moved_arm_base = False
-                #     self.timestep_count = self.FTIME
-                #
-                # el
-                if np.all(self.box.dq < 0.05):
-                    self.skel.set_positions(self.start)
-                    self.skel.world.complete = True
-                    self.flipped = False
-                    self.moved_arm_base = False
-                    self.timestep_count = self.FTIME
-            except:
-                print([bodynode.mass() for bodynode in self.box.bodynodes])
+
+            if np.any(np.isnan(self.box.dq)):
+                print('went NaN',[bodynode.mass() for bodynode in self.box.bodynodes])
+                self.skel.set_positions(self.start)
+                self.skel.world.complete = True
+                self.flipped = False
+                self.moved_arm_base = False
+                self.timestep_count = self.FTIME
+
+            elif np.all(self.box.dq < 0.05):
+                self.skel.set_positions(self.start)
+                self.skel.world.complete = True
+                self.flipped = False
+                self.moved_arm_base = False
+                self.timestep_count = self.FTIME
 
 
             force = self.skel.coriolis_and_gravity_forces()
