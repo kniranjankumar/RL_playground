dac6de3c17b5167c6491c3d5e5d3a21d4fece6c2
Niranjan <kniranjankumar.eee@gmail.com>
2019-06-03 23:41:58 -0400

    'non_deterministic policy execution'

--- stable_baselines/check_arm.py
+++ stable_baselines/check_arm.py
@@ -594,7 +594,7 @@ def evaluate(policy, env):
     rew_list = []
     for i in range(40):
         # act, state = policy.predict(obs, state, done, True)
-        act = env.action_space.sample()
+        act = act = [env.action_space.sample() for j in range(16)]
         print(act.shape)
         obs, rew, done, _ = env.step(act)
         if (np.sum(done) != 0):
