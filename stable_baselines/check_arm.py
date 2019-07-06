ab8c0d32ff77435ab8deb5d000b6f717800c22d2
Niranjan <kniranjankumar.eee@gmail.com>
2019-07-06 08:20:36 -0700

    Update check_arm.py

--- stable_baselines/check_arm.py
+++ stable_baselines/check_arm.py
@@ -694,6 +694,8 @@ parser.add_argument("--mass_range", help='1->[0.1,1] 2->[1,10] 3->[0.1,10]',
                     default=3, type=int,nargs='?', const=3)
 parser.add_argument("--action_scale", help='int between 1 to 5',
                     default=5, type=int,nargs='?', const=5)
+parser.add_argument("--ball_type", help='int 1 or 2',
+                    default=1, type=int,nargs='?', const=1)
 parser.add_argument("--folder_name", help='name of the log folder', default='Exp1', type=str, nargs='?', const='Exp1')
 args = parser.parse_args()
 arguments = get_arguments(mode=args.type)
@@ -701,6 +703,7 @@ arguments['folder_name'] = args.folder_name
 arguments['mass_range_upper'] = 10 if args.mass_range == 2 or args.mass_range == 3 else 1
 arguments['mass_range_lower'] = 0.1 if args.mass_range == 1 or args.mass_range == 3 else 1
 arguments['action_scale'] = args.action_scale
+arguments['ball_type'] = arg.ball_type
 # arguments['chain_length'] = 2
 the_path = os.path.join(path, 'experiments', 'KR5_arm', arguments['folder_name'])
 folders = glob(os.path.join(the_path, '*'))
